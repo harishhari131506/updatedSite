57c2ebe5b618042190e39cc4c18611ae1c23ec0e
harishsify13 <168715357+harishsify13@users.noreply.github.com>
2025-05-07 07:53:46 +0530

    kn

--- src/components/Footer1.jsx
+++ src/components/Footer1.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
         className='relative h-[800px]'
         style={{clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)"}}
     >
-        <div className='relative h-[calc(100vh+800px)] -top-[100vh]'>
+        <div className='relative h-[calc(100vh+800px)] -top-[100vh] bg-[#2D2C43]'>
             <div className='h-[800px] sticky top-[calc(100vh-800px)]'>
                 <Content />
             </div>
